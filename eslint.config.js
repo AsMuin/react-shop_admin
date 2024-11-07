17ef2ad33ca2f666c855faf5bec74123038c1603
AsMuin <asmuins@foxmail.com>
2024-11-07 10:47:27 +0000

    feat: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import prettierPlugin from 'eslint-plugin-prettier';
+import reactPlugin from 'eslint-plugin-react';
 export default tseslint.config(
     { ignores: ['dist'] },
     {
@@ -16,13 +17,21 @@ export default tseslint.config(
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
-            prettier: prettierPlugin
+            prettier: prettierPlugin,
+            react: reactPlugin
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
+            ...reactPlugin.configs.recommended.rules,
+            ...reactPlugin.configs.flat['jsx-runtime'].rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-            '@typescript-eslint/no-explicit-any': 'off'
-            // 'prettier/prettier': 'warn'
+            '@typescript-eslint/no-explicit-any': 'off',
+            'prettier/prettier': 'warn'
+        },
+        settings: {
+            react: {
+                version: 'detect'
+            }
         }
     }
 );
