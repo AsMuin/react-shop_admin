c615116861c2f4b01462a779ee5a9b713f246157
AsMuin <asmuins@foxmail.com>
2024-11-08 01:52:14 +0800

    update: vite_config

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    const comon = {
+    const env = loadEnv(mode, process.cwd(), '');
+    const common = {
         plugins: [react()],
         resolve: {
             alias: {
                 '@': resolve(__dirname, './src')
             }
-        },
-    }
+        }
+    };
     if (command === 'serve') {
         return {
-            ...comon,
+            ...common,
             // dev 独有配置
             server: {
                 proxy: {
@@ -23,12 +23,12 @@ export default defineConfig(({ command, mode }) => {
                     }
                 }
             }
-        }
+        };
     } else {
         // command === 'build'
         return {
-            ...comon
+            ...common
             // build 独有配置
-        }
+        };
     }
-})
+});
